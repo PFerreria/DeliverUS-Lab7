80805dcb2a33661445d32160892839c8dbcac6d5
Pablo Ferreria <pferreriahijon@gmail.com>
2024-04-09 09:24:48 +0200

    Initial commit

--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -1,16 +1,78 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import { StyleSheet } from 'react-native'
 import TextRegular from '../../components/TextRegular'
+import InputItem from '../../components/InputItem'
+import { View, Pressable, StyleSheet } from 'react-native'
+import GlobalStyles from '../../styles/GlobalStyles'
 
 export default function CreateRestaurantScreen () {
   return (
-    <TextRegular textStyle={styles.text}>
-      Create restaurant
-    </TextRegular>
+    <View style={{ alignItems: 'center' }}>
+      <View style={{ width: '60%' }}>
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <InputItem
+        name='sampleInput'
+        label='Sample input'
+      />
+      <Pressable
+        onPress={() => console.log('Button pressed')
+        }
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed
+              ? GlobalStyles.brandPrimaryTap
+              : GlobalStyles.brandPrimary
+          },
+          styles.button
+        ]}>
+        <TextRegular textStyle={styles.text}>
+          Create restaurant
+        </TextRegular>
+      </Pressable>
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  button: {
+    borderRadius: 8,
+    height: 40,
+    padding: 10,
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: GlobalStyles.brandSecondary,
+    textAlign: 'center'
+  }
 })
